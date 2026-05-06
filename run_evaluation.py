3d2ac8e880541fd05e8030c1a21039fe14953906
Cecile Rousseau <rousseau.cecile@outlook.com>
2026-05-06 13:48:17 +0100

    Fix: Rename main files

--- run_evaluation.py
+++ run_evaluation.py
@@ -1,18 +1,18 @@
 import argparse
-from torch.utils.data.dataloader import DataLoader
-from typing import Any
 import yaml
-import torch
 from multiprocessing import cpu_count
 import torch.multiprocessing as mp
+
 from torch.utils.data import DataLoader
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Repo-specific imports
+# ----------------------------------------------------------------------------------------------------------------------
 
 try:
     from globals import REPO_ROOT
 except ImportError:
     from .globals import REPO_ROOT
-
-from utils import set_seed, seed_worker
 
 from tokamark.tools.utils import get_device
 from tokamark.data_split import get_train_test_val_shots
@@ -24,6 +24,13 @@ from tokamark.data import (
     initialize_MAST_dataset, 
     initialize_TokaMark_dataset,
 )
+
+from tokamark.evaluator import (
+    WindowMetricsAccumulator,
+    compute_metrics,
+)
+
+
 from src.cnn_model_v2 import (
     create_cnn_v2_architecture
 )
@@ -36,7 +43,10 @@ from src.model_transform import (
 )
 from src.trainer import (
     model_collate_fn,
-    BatchStepTrainer,
+)
+
+from src.evaluator import (
+    cnn_unstd_evaluation_per_shot,
 )
 
 from tokamark.tools.path import (
@@ -51,18 +61,18 @@ from MAST_tools.utils.path_utils import (
     TEMPORAL_SPLIT_OUTLIER_METADATA_FILE,
     )
 
+
 # ----------------------------------------------------------------------------------------------------------------------
 
 # Set device
 device = get_device()
-# print(f"Using device: {device} and pin_memory is {torch.cuda.is_available()}\n")
+# print(f"Using device: {device}\n")
 
 
 # ======================================================================================================================
 if __name__ == "__main__":
-
     print(f"Number of available CPU cores: {cpu_count()}\n")
-    mp.set_start_method(method="spawn", force=True)
+    mp.set_start_method("spawn", force=True)
 
     # ------------------------------------------------------------------------------------------------------------------
     # Argument parsing
@@ -72,26 +82,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "--task",
         type=str,
-        default="task_4-5",
-        help="The name of the task available in the benchmark"
+        default="task_2-1",
+        help="The name of the task available in the benchmark",
     )
     parser.add_argument(
         "--config",
         type=str,
         default="/src/config/config_model_test.yaml",
-        help="Path to the model YAML config file."
+        help="Path to the model YAML config file",
     )
     parser.add_argument(
         "--seed",
         type=int,
         default=23,
-        help="Specified seed."
-    )
-    parser.add_argument(
-        "--validate_every",
-        type=int,
-        default=100,
-        help="Number of batches at which validation is performed."
+        help="Specified seed"
     )
     parser.add_argument(
         "--model",
@@ -113,56 +117,17 @@ if __name__ == "__main__":
 
     # Load Task YAML config
     config_task = get_task_config(task_name=args.task)
-    print(config_task)
 
     # Load CNN YAML config
     with open(REPO_ROOT + args.config, "r") as f:
         config = yaml.safe_load(f)
-    print(config)
-
+    
     SEED = args.seed
-    set_seed(SEED)
     print(SEED)
-
+    
     # ------------------------------------------------------------------------------------------------------------------
     # Initialize task-specific metadata
     # ------------------------------------------------------------------------------------------------------------------
-
-    # For training and validation: use stride of 0.005ms and 0.025ms
-    if args.task in ["task_3-3",
-                     "task_4-1", "task_4-2",
-                     "task_4-3", "task_4-4", "task_4-5"]:              
-        config_task["stride_window"] = 0.025
-        shuffle_buffer_size = 2048  # Typical options: 512, 2048
-    else:
-        config_task["stride_window"] = 0.005
-        shuffle_buffer_size = 2048
-
-    if args.task in [
-        "task_1-1",
-        "task_1-2", 
-        "task_1-3",
-        "task_2-1",
-        "task_2-2", 
-        "task_2-3",        
-        "task_3-1",
-        "task_3-2", 
-        "task_3-3",
-        "task_4-3"
-        ]:
-        config['dataloader_setting']['num_workers'] = 8
-    
-    elif args.task in [
-        "task_4-1",
-        "task_4-2", 
-        "task_4-4", 
-        "task_4-5"]:
-        config['dataloader_setting']['num_workers'] = 4
-
-    else:
-        print('Task not known!')
-
-    print('num worker to ', config['dataloader_setting']['num_workers'])
 
     dict_task_metadata = get_task_metadata(
         config_task=config_task,
@@ -186,24 +151,23 @@ if __name__ == "__main__":
         SIGNAL_STATS = TEMPORAL_SPLIT_SIGNALS_STATS_FILE
     
     else:
-        raise ValueError('Split Unknkwn')
-        
+        print('SPLIT UNKNOWN')
 
     # ------------------------------------------------------------------------------------------------------------------
     # Initialize MAST datasets
     # ------------------------------------------------------------------------------------------------------------------
 
     train_shots_, test_shots_, val_shots_ = get_train_test_val_shots(
-        max_index=config["subset_of_shots"],
+        max_index=config["subset_of_shots"],        
         shuffle=True,
-        data_splits_file_path = DATA_SPLIT        
+        data_splits_file_path = DATA_SPLIT,
     )
 
     local_flag = config["local"]
 
-    train_MAST_dataset = initialize_MAST_dataset( 
+    test_MAST_dataset = initialize_MAST_dataset( 
         config_task=config_task,
-        shots_list=train_shots_,
+        shots_list=test_shots_,
         local_flag=local_flag,
         use_std_scaling=True,
         stats_metadata_file_path=SIGNAL_STATS,
@@ -212,69 +176,45 @@ if __name__ == "__main__":
         remove_outliers=True,
         outlier_metadata_file=OUTLIER_FILE,
         remove_bad_efit_rating=True,
-        verbose=False
-    )
-    val_MAST_dataset = initialize_MAST_dataset( 
-        config_task=config_task,
-        shots_list=val_shots_,
-        local_flag=local_flag,
-        use_std_scaling=True,
-        stats_metadata_file_path=SIGNAL_STATS,
-        use_nan_filling=False,
-        return_incomplete_shots=True,
-        remove_outliers=True,
-        outlier_metadata_file=OUTLIER_FILE,
-        remove_bad_efit_rating=True,
+        store_manager_settings=config["store_manager_settings"],
         verbose=False
     )
 
     # ------------------------------------------------------------------------------------------------------------------
     # CNN pipeline
     # ------------------------------------------------------------------------------------------------------------------
-
+    
     model_specific_transform = ComposeTransforms(
         [
             ModelTransform_1(dict_task_metadata | config_task),
             ModelTransform_2(dict_task_metadata | config_task),
         ]
     )
-
-    g = torch.Generator()
-    g.manual_seed(SEED)
-
-    train_dataset = initialize_TokaMark_dataset(
-        dataset=train_MAST_dataset,
+    
+    test_dataset = initialize_TokaMark_dataset(
+        dataset=test_MAST_dataset,
         task_metadata=dict_task_metadata,
         config_metadata=config_task,
         custom_transform=model_specific_transform,
-        test_mode=True
+        test_mode=True,
+        shuffle_windows=False
     )
-    train_dataloader: DataLoader[Any] = DataLoader(
-            dataset=train_dataset,
+    
+    if test_dataset is None:
+        raise ValueError("Failed to initialize test dataset. test_MAST_dataset may be None or invalid.")
+    
+    test_dataloader = DataLoader(
+            dataset=test_dataset,
             collate_fn=model_collate_fn,
-            worker_init_fn=seed_worker,
-            generator=g,
             **config["dataloader_setting"],
-            pin_memory=torch.cuda.is_available(),
-            drop_last=True
-        )
-
-    val_dataset = initialize_TokaMark_dataset(
-        dataset=val_MAST_dataset,
-        task_metadata=dict_task_metadata,
-        config_metadata=config_task,
-        custom_transform=model_specific_transform,
-        test_mode=True
-    )
-    val_dataloader = DataLoader(
-            dataset=val_dataset,
+            pin_memory=True,
+        )    
+    test_dataloader_vizu = DataLoader(
+            dataset=test_dataset,
             collate_fn=model_collate_fn,
-            worker_init_fn=seed_worker,
-            generator=g,
-            **config["dataloader_setting"],
-            pin_memory=torch.cuda.is_available()
+            batch_size=1,
+            num_workers=0,
         )
-
     # ------------------------------------------------------------------------------------------------------------------
     # Initialize Model
     # ------------------------------------------------------------------------------------------------------------------
@@ -282,7 +222,7 @@ if __name__ == "__main__":
     if args.model == 'cnn':
 
         model = create_cnn_v2_architecture(
-            dataloader_=train_dataloader,
+            dataloader_=test_dataloader,
             dict_metadata = dict_task_metadata | config_task,
             verbose=True
         )
@@ -290,16 +230,17 @@ if __name__ == "__main__":
     elif args.model == 'lstm':
 
         model = create_lstm_v3_architecture(
-            dataloader_=train_dataloader,
+            dataloader_=test_dataloader,
             dict_metadata = dict_task_metadata | config_task,
             verbose=True
         )
+         
     else:
-        raise ValueError('Model Unknown')
-        
-    # ------------------------------------------------------------------------------------------------------------------
+        print('Model Unknown')
+
+    # -------------------------------------------------------------------
     # Training loop
-    # ------------------------------------------------------------------------------------------------------------------
+    # -------------------------------------------------------------------
 
     base = config["paths"]["data_output_directory"]
 
@@ -309,17 +250,20 @@ if __name__ == "__main__":
         + f"/{args.split}/{args.model}/{config_task['task_name']}/seed_{SEED}/"
     )
 
-    trainer = BatchStepTrainer(
-        model=model,
-        train_loader=train_dataloader,
-        val_loader=val_dataloader,
-        **config["training_args"],
-        output_dir=base_model_dir,
-        device=device,
-        validate_every=args.validate_every,  # Validate every 100 batches by default.
-    )
+    # -------------------------------------------------------------------
+    # Evaluation
+    # -------------------------------------------------------------------
 
-    # Step through batches
-    while trainer.step_batch():
-        # A single pass update is performed as long as `trainer.step_batch()` is True.
-        pass
+    results_dir = REPO_ROOT + f"/results/{args.split}/{args.model}/seed_{SEED}/"
+
+    # cnn_safety_vizu_per_shot(test_dataloader_vizu, config_task, cnn_model, base_model_dir, n_shot_to_plot=3)
+    accumulator = WindowMetricsAccumulator(args.task)
+    cnn_unstd_evaluation_per_shot(test_dataloader, config_task, model, base_model_dir, accumulator)
+    
+    compute_metrics(
+        task=args.task,
+        output_dir=results_dir,
+        window_metrics_accumulator=accumulator,
+        save_windows_metrics=True,
+        save_task_metrics=True,
+    )
